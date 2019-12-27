847823bddd894517aa1ce27825c5ab2c04890a8a
노국현 <YNWA@nogughyeon-ui-MacBook-Pro.local>
2019-12-27 23:10:09 +0900

    portfolio updating...

---
+++ pf-backend/src/api/posts/posts.ctrl.js
@@ -0,0 +1,106 @@
+//id의 초기 값.
+let postId = 1;
+
+const posts = [
+  {
+    id: 1,
+    title: '제목',
+    body: '내용',
+  },
+];
+
+/* 포스트 작성 POST / api/posts {title,body} */
+
+exports.wirte = ctx => {
+  //REST API의 Request Body는 ctx.request.body에서 조회할 수 있다.
+  const { title, body } = ctx.request.body;
+  postId += 1; //기존 포스트값의 1을 더함
+  const post = { id: postId, title, body };
+  posts.push(post);
+  ctx.body = post;
+};
+
+/* 포스트 조회 GET / api/posts */
+exports.list = ctx => {
+  ctx.body = posts;
+};
+
+/* 특정 포스트 조회 GET / api/posts/:id */
+exports.read = ctx => {
+  const { id } = ctx.params;
+  //주어진 id값으로 포스트를 찾는다.
+  //파라미터로 받아 온 값은 문자열 이므로 파라미터를 숫자로 변환하거나
+  //비교할 p.id 값을 문자열로 변경해야 한다.
+  const post = posts.find(p => p.id.toString() === id);
+  //포스트가 없으면 Error를 반환
+  if (!post) {
+    ctx.status = 404;
+    ctx.body = {
+      message: '포스트가 존재하지 않습니다.',
+    };
+
+    return;
+  }
+  ctx.body = post;
+};
+
+/* 특정 포스트 제거 DELETE / api/posts/:id */
+exports.remove = ctx => {
+  const { id } = ctx.params;
+  //해당 id를 가진 post가 몇번째인지 확인
+  const idx = posts.findIndex(p => p.id.toString() === id);
+  //포스트가 없으면 Error를 반환
+  if (idx === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message: '포스트가 존재하지 않습니다.',
+    };
+    return;
+  }
+  //idx번째 아이템을 제거
+  posts.splice(idx, 1);
+  ctx.status = 204; // No Content
+};
+
+/* 포스트 수정(교체) PUT /api/posts/:id {title,body} */
+exports.replace = ctx => {
+  //PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할때 사용
+  const { id } = ctx.params;
+  //해당 id를 가진 post가 몇 번째 인지 확인
+  const idx = posts.findIndex(p => p.id.toString() === id);
+  //포스트가 없으면 Error를 반환
+  if (idx === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message: '포스트가 존재하지 않습니다.',
+    };
+    return;
+  }
+  //전체 객체를 덮어 씌움, 따라서 id를 제외한 기존 정보를 날리고, 객체를 새로 만듦
+  posts[idx] = {
+    id,
+    ...ctx.request.body,
+  };
+  ctx.body = posts[idx];
+};
+
+/* 포스트 수정(특정 필드 변경) PATCH /api/posts/:id {title,body} */
+exports.update = ctx => {
+  const { id } = ctx.params;
+  //해당 id를 가진 posts가 몇번째 인지 확인
+  const idx = posts.findIndex(p => p.id.toString() === id);
+  //포스트가 없으면 Error를 반환
+  if (idx === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message: '포스트가 존재하지 않습니다.',
+    };
+    return;
+  }
+  //기존값에 정보를 덮어 씌움
+  posts[idx] = {
+    ...posts[idx],
+    ...ctx.request.body,
+  };
+  ctx.body = posts[idx];
+};
